74798ec72fe6c9dde237fb272f18b4b0c5e431d2
Mateus Belfort <mateusaguia96@gmail.com>
2023-04-10 11:29:11 -0300

    fist commit
    estruturação inicial

---
+++ index.js
@@ -0,0 +1,49 @@
+const minutesEl = document.querySelector('#minutes')
+const secondsEl = document.querySelector('#seconds')
+const milisecondsEl = document.querySelector('#miliseconds')
+const startBtn = document.querySelector('#startBtn')
+const pausebtn = document.querySelector('#pauseBtn')
+const resumeBtn = document.querySelector('#resumeBtn')
+const resetBtn = document.querySelector('#resetBtn')
+
+let interval;
+let minutes = 0;
+let seconds = 0;
+let miliseconds = 0;
+let isPaused = false;
+
+startBtn.addEventListener("click", startTimer)
+
+function startTimer(){
+
+    interval = setInterval(() => {
+
+        if(!isPaused){
+            miliseconds += 10
+
+            if(miliseconds === 1000) {
+                seconds++;
+                miliseconds = 0;
+            }
+
+            if(seconds === 60) {
+                minutes++;
+                seconds = 0;
+            }
+
+            minutesEl.textContent = formatTime(minutes);
+            secondsEl.textContent = formatTime(seconds);
+            milisecondsEl.textContent = formatMiliseconds(miliseconds);
+        }
+
+    }, 10)
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+
+function formatMiliseconds(time) {
+    return time < 100 ? `${time}`.padStart(3, "0") : time;
+}
